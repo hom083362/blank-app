36d681d4fd56439c27c40337d019826dcf124b07
hom083362 <86873379+hom083362@users.noreply.github.com>
2025-06-11 15:56:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+print('TEST')
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
